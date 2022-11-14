6fba992c9152dbac16bbfa088ab07f6dc5549d72
MrSteradian360 <mduma@student.agh.edu.pl>
2022-11-14 23:57:57 +0100

    lab4
    lab4

---
+++ oolab/src/main/java/agh/ics/oop/SimulationEngine.java
@@ -0,0 +1,29 @@
+package agh.ics.oop;
+
+public class SimulationEngine implements IEngine{
+    private MoveDirection[] moveDirection;
+    private IWorldMap map;
+    private Vector2d[] positions;
+
+    public SimulationEngine(MoveDirection[] moveDirection, IWorldMap map, Vector2d[] positions){
+        this.map = map;
+        this.moveDirection = moveDirection;
+        this.positions = positions;
+        for (Vector2d v : positions){
+            map.place(new Animal(map, v));
+        }
+    }
+
+    @Override
+    public void run() {
+        System.out.println(positions.length);
+        System.out.println(moveDirection.length);
+        Vector2d[] pos = positions;
+        for (int i = 0; i < this.moveDirection.length; i++){
+            Animal a = ((Animal) map.objectAt(pos[i % positions.length]));
+            a.move(moveDirection[i]);
+            pos[i % positions.length] = a.getPosition();
+            //System.out.println(a.getPosition());
+        }
+    }
+}
--- oolab/src/main/java/agh/ics/oop/World.java
+++ oolab/src/main/java/agh/ics/oop/World.java
@@ -35,50 +35,63 @@ public class World {
     }
 
     public static void main(String[] args) {
-        System.out.println("Start");
-        MoveDirection[] directions = change(args);
-        run(directions);
-        System.out.println("Stop");
-        System.out.println();
-
-        Vector2d position1 = new Vector2d(1,2);
-        System.out.println(position1);
-        Vector2d position2 = new Vector2d(-2,1);
-        System.out.println(position2);
-        System.out.println(position1.add(position2));
-
-        MapDirection md1 = MapDirection.WEST;
-        System.out.println();
-        System.out.println(md1);
-        System.out.println(md1.next());
-        System.out.println(md1.previous());
-        System.out.println(md1.toUnitVector());
-
-        System.out.println();
-        System.out.println(new Animal());
-
-        System.out.println();
-        Animal animal1 = new Animal();
-        System.out.println("Start");
-        animal1.move(MoveDirection.RIGHT);
-        animal1.move(MoveDirection.FORWARD);
-        animal1.move(MoveDirection.FORWARD);
-        animal1.move(MoveDirection.FORWARD);
-        System.out.println("Stop");
-
-        Animal animal2 = new Animal();
-        OptionsParser parser = new OptionsParser();
-        System.out.println();
-        System.out.println("Start");
-        MoveDirection[] directions1 = parser.parse(new String[]{"r", "f", "f", "f", "s", "p"});
-        for(MoveDirection direction : directions1) animal2.move(direction);
-        System.out.println("Stop");
-        System.out.println();
+//        System.out.println("Start");
+//        MoveDirection[] directions = change(args);
+//        run(directions);
+//        System.out.println("Stop");
+//        System.out.println();
+//
+//        Vector2d position1 = new Vector2d(1,2);
+//        System.out.println(position1);
+//        Vector2d position2 = new Vector2d(-2,1);
+//        System.out.println(position2);
+//        System.out.println(position1.add(position2));
+//
+//        MapDirection md1 = MapDirection.WEST;
+//        System.out.println();
+//        System.out.println(md1);
+//        System.out.println(md1.next());
+//        System.out.println(md1.previous());
+//        System.out.println(md1.toUnitVector());
+//
+//        System.out.println();
+//        System.out.println(new Animal());
+//
+//        System.out.println();
+//        Animal animal1 = new Animal();
+//        System.out.println("Start");
+//        animal1.move(MoveDirection.RIGHT);
+//        animal1.move(MoveDirection.FORWARD);
+//        animal1.move(MoveDirection.FORWARD);
+//        animal1.move(MoveDirection.FORWARD);
+//        System.out.println("Stop");
+//
+//        Animal animal2 = new Animal();
+//        OptionsParser parser = new OptionsParser();
+//        System.out.println();
+//        System.out.println("Start");
+//        MoveDirection[] directions1 = parser.parse(new String[]{"r", "f", "f", "f", "s", "p"});
+//        for(MoveDirection direction : directions1) animal2.move(direction);
+//        System.out.println("Stop");
+//        System.out.println();
 
         //lab3_zad 10: dodać kolekcję grupującą współrzędne zwierząt i dodać warunek do ifa dla case FORWARD i BACKWARD
         // w switchu w metodzie main sprwdzajcego czy nowego położenia zwierzęcia nie ma w kolekcji, jeśli nie to
         // przesuwamy zwierzę na nową pozycję i dodajemy nowe koordynaty do kolekcji, jeśli  jest to nie wykonujemy
         // przejścia
+
+//        RectangularMap rectangularMap = new RectangularMap(4,4);
+//        System.out.println(rectangularMap);
+
+        MoveDirection[] directions = new OptionsParser().parse(args);
+        IWorldMap map = new RectangularMap(10, 5);
+        Vector2d[] positions = { new Vector2d(2,2), new Vector2d(3,4) };
+        IEngine engine = new SimulationEngine(directions, map, positions);
+        engine.run();
+        System.out.println(map);
+//        Animal a = new Animal(map, new Vector2d(2,2));
+//        map.place(a);
+//        System.out.println(map.objectAt(new Vector2d(2,2)));
     }
 
 
